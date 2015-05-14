c1ec05a15cc412881e0897837b7a9a0bb552a302
TravisCG <buldozer88@hotmail.com>
2015-05-14 06:21:41 +0000

    Better limit calculation

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "random.h"
 
 #define SIMNUM 100e6
@@ -88,13 +89,20 @@ int main(int argc, char **argv){
 		return(2);
 	}
 	readtable(argv[1]);
-	limit = atof(argv[2]);
 	initrnd();
 	copy2vec();
 
 	o1 = mean(0, count);
 	o2 = mean(count, count * 2);
 	o3 = mean(count * 2, count * 3);
+
+	limit = fabs(o1 - o2) / 2.0 - 0.0001;
+	if( fabs(o1 - o3) / 2.0 - 0.0001 < limit){
+		limit = fabs(o1 - o3) / 2.0 - 0.0001;
+	}
+	if( fabs(o2 - o3) / 2.0 - 0.0001 < limit){
+		limit = fabs(o2 - o3) / 2.0 - 0.0001;
+	}
 
 	for(i = 0; i < SIMNUM; i++){
 		m1 = mean(0, count);
